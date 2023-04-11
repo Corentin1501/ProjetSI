031531de7cd0c84b60738fd35240938639bef6ac
Corentin Cuvelier <corentin.cuvelier49@gmail.com>
2023-04-11 14:44:58 +0200

    modif voiture

--- main.c
+++ main.c
@@ -15,8 +15,6 @@
 #include "stdbool.h"
 #include "ppm.h"
 
-
-
 float angle_rotation = 0;   // rotation personnage deplacement
 float coordonnee_x   = 0;   // deplacement en x du personnage
 float coordonnee_z   = 0;   // deplacement en z du personnage
@@ -31,48 +29,114 @@ float coordonnee_coureur_z  = 0;
 static const int SCENE_SIZE = 35;   // taille de la scène
 
 
+static int faces[][4] = {{0, 1, 2, 3}, {4, 5, 6, 7}, {1, 4, 7, 2}, {0, 3, 6, 5}, {2, 7, 6, 3}, {1, 0, 5, 4}};
+static int sommets[][3] = {{0, 3, 5}, {0, 2, 5}, {0, 2, 4}, {0, 3, 4}, {1, 2, 5}, {1, 3, 5}, {1, 3, 4}, {1, 2, 4}};
+
+
+struct point { GLfloat x,y,z; };
+struct cube { 
+    struct point sommets[8]; 
+};
+
+struct cube creer_cube1(float taille)
+{
+    // struct cube* un_cube = (struct cube*)malloc(sizeof(struct cube));
+    struct cube un_cube;
+    
+    un_cube.sommets[0].x = -taille/2;
+    un_cube.sommets[0].y = -taille/2;
+    un_cube.sommets[0].z = taille/2;
+    
+    un_cube.sommets[1].x = taille/2;
+    un_cube.sommets[1].y = -taille/2;
+    un_cube.sommets[1].z = taille/2;
+
+    un_cube.sommets[2].x = taille/2;
+    un_cube.sommets[2].y = taille/2;
+    un_cube.sommets[2].z = taille/2;
+
+    un_cube.sommets[3].x = -taille/2;
+    un_cube.sommets[3].y = taille/2;
+    un_cube.sommets[3].z = taille/2;
+
+    un_cube.sommets[4].x = taille/2;
+    un_cube.sommets[4].y = -taille/2;
+    un_cube.sommets[4].z = -taille/2;
+
+    un_cube.sommets[5].x = -taille/2;
+    un_cube.sommets[5].y = -taille/2;
+    un_cube.sommets[5].z = -taille/2;
+
+    un_cube.sommets[6].x = -taille/2;
+    un_cube.sommets[6].y = taille/2;
+    un_cube.sommets[6].z = -taille/2;
+
+    un_cube.sommets[7].x = taille/2;
+    un_cube.sommets[7].y = taille/2;
+    un_cube.sommets[7].z = -taille/2;
+
+    return un_cube;
+}
+struct voiture {
+    struct cube bottom;
+    struct cube top;
+};
+
+struct voiture creerVoiture(float taille)
+{
+    struct voiture uneVoiture;
+
+    uneVoiture.bottom = creer_cube1(taille);
+    uneVoiture.top = creer_cube1(taille/2);
+
+    return uneVoiture;
+}
+
+struct voiture* Creation_Voitures()
+{
+    struct voiture* voitures = malloc(1 * sizeof(struct voiture)); // alloue la mémoire pour le tableau de voitures
+
+    struct voiture uneVoiture = creerVoiture(1);
+
+    voitures[0] = uneVoiture;
+
+    return voitures;
+}
+
+void affiche_cube1(struct cube un_cube)
+{
+    int j = 0;
+    for(j = 0; j <= 5; j++) 
+    {
+
+        glBegin(GL_QUADS);
+        int i = 0;
+        for (i = 0; i <= 3; i++) 
+        {
+            glVertex3f( un_cube.sommets[faces[j][i]].x,
+                        un_cube.sommets[faces[j][i]].y,
+                        un_cube.sommets[faces[j][i]].z);
+        }
+        glEnd();
+    }																																																																																																																																																																																																																																																										
+}
+
+void afficherVoiture(struct voiture une_voiture)
+{
+    affiche_cube1(une_voiture.bottom);
+    affiche_cube1(une_voiture.top);
+}
+
 GLvoid Modelisation()
 {
     VM_init();
 
-    glPushMatrix();
-    {
-        glTranslatef(-((SCENE_SIZE/2)-4), 0, ((SCENE_SIZE/2)-4));
-        glTranslatef(coordonnee_coureur_x, 0, coordonnee_coureur_z);    
-        glRotatef(rotation_coureur_y, 0, 1, 0);             // translation et rotation causée par le déplacement du personnage
-        creer_personnage(angle_coureur_deplacement);
-    }
-    glPopMatrix();
+    glColor3f(1, 60/255.0, 65/255.0);
 
-    glPushMatrix();
-    {
+    struct voiture* voitures = Creation_Voitures();
 
-        creer_sol(SCENE_SIZE);
-        creer_maison(SCENE_SIZE);
+    afficherVoiture(voitures[0]);
 
-        glTranslatef(10,0,10);
-        creer_bucheron();
-        glTranslatef(0,0,4);
-
-        creer_buche();
-        
-        glTranslatef(-10,0,-14);
-
-        // empecher le personnage de sortir de la scène
-            int border = SCENE_SIZE/2 - 1;
-            if(coordonnee_x > border) coordonnee_x = border;
-            if(coordonnee_x < -border) coordonnee_x = -border;
-            if(coordonnee_z > border) coordonnee_z = border;
-            if(coordonnee_z < -border) coordonnee_z = -border;
-
-        glTranslatef(coordonnee_x, 0, coordonnee_z);    
-        glRotatef(angle_rotation, 0, 1, 0);             // translation et rotation causée par le déplacement du personnage
-        creer_personnage(angle_deplacement);
-
-    }
-    glPopMatrix();
-
-    creer_fenetres(SCENE_SIZE);
 
     axes();
     glutSwapBuffers();
